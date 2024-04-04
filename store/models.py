32bb4a93eac8f3e46d4bfaa46a3898895dca12c0
Adam Fournace <fournacea@gmail.com>
2024-04-04 16:32:33 -0400

    ADD on_sale and sale_price to Products model

--- store/models.py
+++ store/models.py
@@ -34,6 +34,9 @@ class Product(models.Model):
     description = models.CharField(max_length=300, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    # Sales
+    on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     def __str__(self):
         return self.name
 
